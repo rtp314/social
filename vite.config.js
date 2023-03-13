89ffc7e9896829db7d4c6a817a3960df510e0cc0
Samuel <samuelreed01@gmail.com>
2023-03-13 22:04:53 +0700

    change base url

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/social/",
 	plugins: [react()],
 	build: { rollupOptions: { 
     output: { manualChunks: { firestore: ["firebase/firestore"] } } }, target: "modules", outDir: 'docs' },
